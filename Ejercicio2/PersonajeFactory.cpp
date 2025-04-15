4a433a9009dd09274e94c06a9fbf4a2ec5b5aec9
Fariascamilas <cfarias@udesa.edu.ar>
2025-04-15 19:15:12 -0300

    Ejercicio2 terminado

--- Ejercicio2/PersonajeFactory.cpp
+++ Ejercicio2/PersonajeFactory.cpp
@@ -1,88 +1,76 @@
 #include "PersonajeFactory.hpp"
 #include "CharacterGenerator.hpp"
+#include "../Ejercicio1/Arma/ArmaDeCombate.hpp"
+#include "../Ejercicio1/Arma/ItemMagico.hpp"
+#include "../Ejercicio1/Personaje/Guerrero.hpp"
+#include "../Ejercicio1/Personaje/Mago.hpp"
 #include <iostream>
+#include <vector>
 using namespace std;
 
-unique_ptr<Guerrero> PersonajeFactory::createGuerrero(const array<IArma*,2>& weapons, int type){
-    switch(type){
-        case BARBARO:
-            return make_unique<Barbaro>("Barbaro","",weapons); // Lo ideal sería que lleven diversos nombres, pero es innecesario en este caso
-        case PALADIN:
-            return make_unique<Paladin>("Paladin","",weapons);
-        case CABALLERO:
-            return make_unique<Caballero>("Caballero","",weapons);
-        case MERCENARIO:
-            return make_unique<Mercenario>("Mercenario","",weapons);
-        case GLADIADOR:
-            return make_unique<Gladiador>("Gladiador","",weapons);
-        default:
-            throw invalid_argument("Invalid character option");
-    }
-}
+std::vector<std::string> names = {"Aelric", "Thalor", "Kaelwyn", "Dareth", "Varek", "Elandor", "Maelis", "Draven", "Nyros", "Zarion", "Vaelric", "Korwyn", "Sylthar", "Orimar", "Rhaziel", "Tarnak", "Zephir", "Malrik", "Fenros", "Caldrin", "Arenvar", "Myrden", "Kaedor", "Velmorr", "Thandor", "Eryndor", "Galrik", "Sorveth", "Zarnak", "Lareth", "Branor", "Vaedin", "Nerys", "Khorlan", "Syvar", "Drelkar", "Faelric", "Xandor", "Tyveris", "Orren"};
 
-unique_ptr<Mago> PersonajeFactory::createMago(const array<IArma*,2>& weapons, int type){
+std::vector<std::string> habilities = {"Despertar", "Esencia", "Resonancia", "Sombra", "Impulso", "Eco", "Luz", "Vínculo", "Voluntad", "Furia", "Silencio", "Ascenso", "Colapso", "Guardia", "Llamado", "Aliento", "Destino", "Amanecer", "Caída"};
+
+std::vector<std::string> namesWeapons = {"Umbra", "Ignaris", "Velanth", "Thoros", "Zephyrion", "Lunaris", "Duskbane", "Solmara", "Ebonflare", "Mystral", "Nerion", "Vaelshade", "Pyrralis", "Oreth", "Drakmor", "Nytheris", "Glimveil", "Xarn", "Volraith", "Aetherin"};
+
+std::vector<std::string> oddities = {"Común", "Poco Común", "Rara", "Épica", "Legendaria", "Mítica", "Ancestral", "Arcana", "Maldita", "Divina", "Única", "Desconocida", "Inestable", "Forjada", "Perdida", "Primordial", "Infame", "Corrupta", "Celestial", "Eterna"};
+
+
+unique_ptr<IPersonaje> PersonajeFactory::createPersonaje(vector<unique_ptr<IArma>> weapons, int type){
     switch(type){
         case HECHICERO:
-            return make_unique<Hechicero>("Hechicero","",weapons); 
+            return make_unique<Hechicero>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons)); 
         case CONJURADOR:
-            return make_unique<Conjurador>("Conjurador","",weapons);
+            return make_unique<Conjurador>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
         case BRUJO:
-            return make_unique<Brujo>("Brujo","",weapons);
+            return make_unique<Brujo>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
         case NIGROMANTE:
-            return make_unique<Nigromante>("Nigromante","",weapons);
+            return make_unique<Nigromante>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
+        case BARBARO:
+            return make_unique<Barbaro>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons)); 
+        case PALADIN:
+            return make_unique<Paladin>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
+        case CABALLERO:
+            return make_unique<Caballero>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
+        case MERCENARIO:
+            return make_unique<Mercenario>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
+        case GLADIADOR:
+            return make_unique<Gladiador>(names[randomNumber(0,names.size()-1)], habilities[randomNumber(0, habilities.size()-1)],move(weapons));
         default:
-            throw invalid_argument("Invalid character option");
+            return nullptr;
     }
 }
 
-unique_ptr<ArmaDeCombate> PersonajeFactory::createArmadeCombate(int type){
-    switch(type){
-        case HACHA_SIMPLE:
-            return make_unique<HachaSimple>("HachaSimple",""); 
-        case HACHA_DOBLE:
-            return make_unique<HachaDoble>("HachaDoble","");
-        case ESPADA:
-            return make_unique<Espada>("Espada","");
-        case LANZA:
-            return make_unique<Lanza>("Lanza","");
-        case GARROTE:
-            return make_unique<Garrote>("Garrote","");
-        default:
-            throw invalid_argument("Invalid character option");
-    }
-}
-
-unique_ptr<ItemMagico> PersonajeFactory::createItemMagico(int type){
+unique_ptr<IArma> PersonajeFactory::createArma(int type){
     switch(type){
         case BASTON:
-            return make_unique<Baston>("Baston",""); 
+            return make_unique<Baston>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]); 
         case LIBRO_DE_HECHIZOS:
-            return make_unique<LibroDeHechizos>("LibroDeHechizos","");
+            return make_unique<LibroDeHechizos>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
         case POSION:
-            return make_unique<Posion>("Posion","");
+            return make_unique<Posion>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
         case AMULETO:
-            return make_unique<Amuleto>("Amuleto","");
+            return make_unique<Amuleto>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
+        case HACHA_SIMPLE:
+            return make_unique<HachaSimple>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]); 
+        case HACHA_DOBLE:
+            return make_unique<HachaDoble>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
+        case ESPADA:
+            return make_unique<Espada>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
+        case LANZA:
+            return make_unique<Lanza>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
+        case GARROTE:
+            return make_unique<Garrote>(namesWeapons[randomNumber(0,namesWeapons.size()-1)], oddities[randomNumber(0, oddities.size()-1)]);
         default:
-            throw invalid_argument("Invalid character option");
+            return nullptr;
     }
 }
 
-unique_ptr<Guerrero> PersonajeFactory::createArmedGuerrero(int typeCharacter){
-    int numWeapons = cantArmas();
-    array<IArma*,2> weapons = {nullptr, nullptr};
-    for(int i = 0; i<numWeapons; i++){
-        unique_ptr<ArmaDeCombate> weapon = createArmadeCombate(randomNumber(0,4));
-        weapons[i] = weapon.release();
-    }
-    return createGuerrero(weapons, typeCharacter);
-}
-
-unique_ptr<Mago> PersonajeFactory::createArmedMago(int typeCharacter){
-    int numWeapons = cantArmas();
-    array<IArma*,2> weapons = {nullptr, nullptr};
-    for(int i = 0; i<numWeapons; i++){
-        unique_ptr<ItemMagico> weapon = createItemMagico(randomNumber(0,3));
-        weapons[i] = weapon.release();
-    }
-    return createMago(weapons, typeCharacter);
+unique_ptr<IPersonaje> PersonajeFactory::createArmedPersonaje(int typeCharacter){
+    std::vector<unique_ptr<IArma>> weapons;
+    weapons.push_back(PersonajeFactory::createArma(randomNumber(0,8)));
+    weapons.push_back(PersonajeFactory::createArma(randomNumber(0,8)));
+    auto newpersonaje=createPersonaje(move(weapons), typeCharacter);
+    return newpersonaje;
 }
--- Ejercicio2/PersonajeFactory.hpp
+++ Ejercicio2/PersonajeFactory.hpp
@@ -1,5 +1,7 @@
 #pragma once
 #include <memory>
+#include <vector>
+#include <string>
 #include "../Ejercicio1/Arma/ArmaDeCombate.hpp"
 #include "../Ejercicio1/Arma/ItemMagico.hpp"
 #include "../Ejercicio1/Personaje/Guerrero.hpp"
@@ -7,11 +9,11 @@
 
 class PersonajeFactory{
 public:
-    static std::unique_ptr<Guerrero> createGuerrero(const std::array<IArma*,2>& weapons, int type);
-    static std::unique_ptr<Mago> createMago(const std::array<IArma*,2>& weapons, int type);
-    static std::unique_ptr<ArmaDeCombate> createArmadeCombate(int type);
-    static std::unique_ptr<ItemMagico> createItemMagico(int type);
+    //static std::unique_ptr<Guerrero> createGuerrero(const std::array<IArma*,2>& weapons, int type);
+    static std::unique_ptr<IPersonaje> createPersonaje(std::vector<std::unique_ptr<IArma>> weapons, int type);
+    //static std::unique_ptr<ArmaDeCombate> createArmadeCombate(int type);
+    static std::unique_ptr<IArma> createArma(int type);
 
-    static std::unique_ptr<Guerrero> createArmedGuerrero(int typeCharacter);
-    static std::unique_ptr<Mago> createArmedMago(int typeCharacter);
+    //static std::unique_ptr<Guerrero> createArmedGuerrero(int typeCharacter);
+    static std::unique_ptr<IPersonaje> createArmedPersonaje(int typeCharacter);
 };
